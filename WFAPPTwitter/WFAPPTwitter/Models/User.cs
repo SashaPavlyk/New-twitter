8693759c3c1dff45dea55a1918721da4e1e1e381
Enot <natasha.cat982404@gmail.com>
2019-11-26 14:01:19 +0200

    Added Class User

---
+++ WFAPPTwitter/WFAPPTwitter/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFAPPTwitter.Models
+{
+    public class User
+    {
+        public string ImagePath { get; set; }
+        public string Name{ get;set;}
+        public int Age { get; set; }
+    }
+}
